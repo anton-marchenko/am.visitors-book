1375166e893679d7b7b6495a573bba4d569d857a
anton-marchenko <fattan@ya.ru>
2020-01-11 13:35:49 +0300

    Add test todos

---
+++ tests/integration/routes/users.test.js
@@ -0,0 +1,8 @@
+describe('/api/users', () => {
+    describe('GET /', () => {
+        it.todo('should return 401 if client is not logged in');
+        it.todo('should return 403 if user is not admin');
+        it.todo('should return 200 if it is a valid request');
+        it.todo('should return all users');
+    });
+});
