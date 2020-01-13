5d34b92de7e08ba8101acfa6919c66aa4f76fd36
anton-marchenko <fattan@ya.ru>
2020-01-13 08:14:14 +0300

    Add tests for allowed-for middleware

--- tests/unit/middleware/allowed-for.test.js
+++ tests/unit/middleware/allowed-for.test.js
@@ -1,4 +1,45 @@
+const allowedFor = require('../../../middleware/allowed-for');
+
 describe('allowedFor middleware', () => {
-    it.todo('should return 403 if user does not have given roles');
-    it.todo('should call next if user has some of given roles')
+
+    let next,
+        send,
+        status,
+        userRoles;
+
+    const exec = () => {
+        const roles = [
+            'role1',
+            'role2'
+        ];
+        next = jest.fn();
+        const req = {
+            user: {
+                roles: userRoles
+            }
+        }
+        send = jest.fn();
+        status = jest.fn().mockReturnValue({ send });
+        const res = { status };
+
+        allowedFor(roles)(req, res, next);
+    }
+
+    it('should return 403 if user does not have any of the given roles', () => {
+        userRoles = [];
+
+        exec();
+
+        expect(status).toHaveBeenCalled();
+        expect(send).toHaveBeenCalled();
+        expect(status.mock.calls[0][0]).toBe(403);
+    });
+
+    it('should call next if user has some of the given roles', () => {
+        userRoles = ['role1'];
+
+        exec();
+
+        expect(next).toHaveBeenCalled();
+    });
 });
