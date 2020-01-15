cdb3f58ffd668ebee52796795011ddfb78f610c3
anton-marchenko <fattan@ya.ru>
2020-01-15 08:10:53 +0300

    Add validate ObjectId middleware

---
+++ tests/unit/middleware/validate-object-id.test.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const validateObjectId = require('../../../middleware/validate-object-id');
+
+describe('validateObjectId middleware', () => {
+
+    let id,
+        status,
+        send,
+        next;
+
+    const exec = () => {
+        const req = {
+            params: {
+                id
+            }
+        };
+        next = jest.fn();
+        send = jest.fn();
+        status = jest.fn().mockReturnValue({ send });
+        const res = { status };
+
+        validateObjectId(req, res, next);
+    }
+
+    it('should return 404 if there is not a valid ObjectId in request', () => {
+        id = '1'; 
+
+        exec();
+
+        expect(status).toHaveBeenCalled();
+        expect(send).toHaveBeenCalled();
+        expect(status.mock.calls[0][0]).toBe(404);
+    });
+
+    it('should call next if there is a valid id in request', () => {
+        id = mongoose.Types.ObjectId().toHexString();
+
+        exec();
+
+        expect(next).toHaveBeenCalled();
+    });
+});
