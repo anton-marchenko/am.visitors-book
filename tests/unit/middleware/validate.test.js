e32644d3e643b3d7acb447bcb1942b51ea4bf02d
anton-marchenko <fattan@ya.ru>
2020-01-19 19:20:34 +0300

    Add tests for validate middleware

---
+++ tests/unit/middleware/validate.test.js
@@ -0,0 +1,50 @@
+const { validate } = require('../../../middleware');
+
+describe('validate middleware', () => {
+    let validatorReturn,
+        validator,
+        status,
+        send,
+        next;
+    const reqBody = 'test';
+
+    const exec = () => {
+        validator = jest.fn().mockReturnValue(validatorReturn);
+        const req = {
+            body: reqBody
+        };
+        next = jest.fn();
+        send = jest.fn();
+        status = jest.fn().mockReturnValue({ send });
+        const res = { status };
+
+        validate(validator)(req, res, next);
+    }
+
+    it('should return 400 if there is an invalid input', () => {
+        validatorReturn = {
+            error: {
+                details: [{ message: 'error' }]
+            }
+        };
+
+        exec();
+
+        expect(validator).toHaveBeenCalled();
+        expect(validator.mock.calls[0][0]).toBe(reqBody);
+        expect(send).toHaveBeenCalled();
+        expect(status.mock.calls[0][0]).toBe(400);
+    });
+
+    it('should call next() if there is a valid input', () => {
+        validatorReturn = {
+            error: undefined
+        };
+
+        exec();
+
+        expect(validator).toHaveBeenCalled();
+        expect(validator.mock.calls[0][0]).toBe(reqBody);
+        expect(next).toHaveBeenCalled();
+    });
+});
