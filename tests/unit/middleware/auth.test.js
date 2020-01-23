c1d227111e87ce4801afe9eaa630e1647ddee0f6
anton-marchenko <fattan@ya.ru>
2020-01-23 15:59:46 +0300

    Refactor tests

--- tests/unit/middleware/auth.test.js
+++ tests/unit/middleware/auth.test.js
@@ -3,23 +3,38 @@ const { User } = require('../../../models/user');
 const { auth } = require('../../../middleware');
 
 describe('auth middleware', () => {
-    it('should populate req.user with the payload of a valid JWT', () => {
-        const user = {
+    let next,
+        send,
+        status,
+        req,
+        user,
+        token;
+
+    beforeEach(() => {
+        user = {
             _id: mongoose.Types.ObjectId().toHexString(),
             roles: [
                 'role1',
                 'role2'
             ]
         };
-        const token = new User(user).generateAuthToken();
-        const req = {
+        token = new User(user).generateAuthToken(); 
+    });
+
+    const exec = () => {
+        req = {
             header: jest.fn().mockReturnValue(token)
         };
         const res = {};
         const next = jest.fn();
 
         auth(req, res, next);
+    }
 
+    it('should populate req.user with the payload of a valid JWT', () => {
+        exec();
+
+        expect(req).toHaveProperty('user');
         expect(req.user).toMatchObject(user);
     });
 });
