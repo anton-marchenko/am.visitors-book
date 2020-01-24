a8fa6296f3d8b0a323c38a370dc0e59fb0d6349d
anton-marchenko <fattan@ya.ru>
2020-01-24 16:56:58 +0300

    Add todos for visit test covering

---
+++ tests/integration/models/visit.test.js
@@ -0,0 +1,6 @@
+const { Visit } = require('../../../models/visit');
+
+describe('visit - createNewVisit()', () => {
+    it.todo('should save visit if it is valid input');
+    it.todo('should return an error if an user is not found for given cardId');
+});
