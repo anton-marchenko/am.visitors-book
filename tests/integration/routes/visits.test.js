7ee6e8e7630274c221dd69ee493bbf9981787ef7
anton-marchenko <fattan@ya.ru>
2020-01-25 13:22:31 +0300

    Add visit tests todos

---
+++ tests/integration/routes/visits.test.js
@@ -0,0 +1,12 @@
+
+describe('/api/visits', () => {
+    
+    describe('POST /', () => {
+        it.todo('should return 201 if new visit has been successfully created');
+        it.todo('should return new visit if input is valid');
+        it.todo('should return 400 if the token is not valid');
+        it.todo('should return 401 if the client is not logged in');
+        it.todo('should return 403 if permission denied');
+        it.todo('should return 404 if an user has not been found for given card ID');
+    });
+});
