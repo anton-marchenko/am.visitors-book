963ba728987c66899e0e91de27895891c6059ec4
anton-marchenko <fattan@ya.ru>
2020-01-27 18:53:36 +0300

    Tests: add tokens tests todos

--- tests/integration/routes/access.test.js
+++ tests/integration/routes/access.test.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 let server;
 
-describe('/api/access', () => {
+describe('/api/access/tokens/validate', () => {
 
     beforeEach(async () => {
         server = require('../../../index');
@@ -13,16 +13,37 @@ describe('/api/access', () => {
         await server.close();
     });
 
-    describe('POST /validate-token', () => {
+    describe('POST /', () => {
         it.todo('should return false if the token is not valid');
         it.todo('should return true if the token is valid');
     });
 
-    describe('POST /third-party-app-token', () => {
+});
+
+describe('/api/access/third-party-app/tokens', () => {
+    describe('GET /', () => {
+        it.todo('should return all access records');
+        it.todo('should return 400 if clients token is not valid');
+        it.todo('should return 401 if a client is not logged in');
+        it.todo('should return 403 if permission denied');
+    });
+
+    describe('POST /', () => {
         it.todo('should return new third party app token');
         it.todo('should return 400 if clients token is not valid');
         it.todo('should return 401 if a client is not logged in');
         it.todo('should return 403 if permission denied');
     });
 
+    describe('DELETE /:accessId', () => {
+        it.todo('should delete the access record if input is valid');
+        it.todo('should return the removed access record');
+
+        it.todo('should return 400 if clients token is not valid');
+        it.todo('should return 401 if a client is not logged in');
+        it.todo('should return 403 if permission denied');
+
+        it.todo('should return 404 if accessId is not valid');
+        it.todo('should return 404 if accessId is not found');
+    });
 });
