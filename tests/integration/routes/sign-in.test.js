1a4690617b5f89868576717c4f450d741261a204
anton-marchenko <fattan@ya.ru>
2020-01-31 19:34:41 +0300

    Tests: add todos for sign-in route

---
+++ tests/integration/routes/sign-in.test.js
@@ -0,0 +1,7 @@
+
+describe('/api/sign-in', () => {
+    it.todo('should return new JWT if input is valid');
+    it.todo('should return 400 if input is not valid');
+    it.todo('should return 400 if an user was not found');
+    it.todo('should return 400 if a password is not valid');
+});
