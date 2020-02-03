c045667ca97800f78976721d4a83e76266145e12
anton-marchenko <fattan@ya.ru>
2020-02-03 17:59:48 +0300

    Feat: add sing-in route

--- routes/sign-in.js
+++ routes/sign-in.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const { validate } = require('../middleware');
+const { signInUserValidator, validatePassword, User } = require('../models/user');
 
-router.post('/', async (req, res) => {
-    res.send('OK');
+const ERROR_MSG = 'Invalid email or password';
+
+router.post('/', validate(signInUserValidator), async (req, res) => {
+    const user = await User.findOne({ login: req.body.login });
+
+    if (!user) return res.status(400).send(ERROR_MSG);
+
+    isPasswordValid = await validatePassword(req.body.password, user.password);
+    if (!isPasswordValid) return res.status(400).send(ERROR_MSG);
+
+    const token = user.generateAuthToken();
+    res.send(token);
 });
 
 module.exports = router;
--- tests/integration/routes/sign-in.test.js
+++ tests/integration/routes/sign-in.test.js
@@ -1,33 +1,82 @@
+const config = require('config');
 const request = require('supertest');
 const jwt = require('jsonwebtoken');
 const { User } = require('../../../models/user');
 
 let server;
 
+const mockUserData = () => ({
+    name: {
+        first: 'test',
+        patronymic: 'test',
+        last: 'test'
+    },
+    password: '12345',
+    login: 'test'
+});
+
 describe('/api/sign-in', () => {
+    let login, password;
 
     beforeEach(async () => {
         server = require('../../../index');
+
+        const userData = mockUserData();
+        await User.createNewUser(userData);
+
+        login = userData.login;
+        password = userData.password;
     });
 
     afterEach(async () => {
         await server.close();
+        await User.deleteMany({});
     });
 
     const exec = async () => {
         return await request(server)
             .post('/api/sign-in')
-            .send({});
+            .send({
+                login,
+                password
+            });
     };
 
-    it('should return new JWT if input is valid', async () => {
+    it('should return valid JWT', async () => {
+        const res = await exec();
+
+        const decoded = jwt.verify(res.text, config.get('jwtSecret'))
+
+        expect(decoded).toBeTruthy();
+    });
+
+    it('should return 200 if input is valid', async () => {
         const res = await exec();
 
         expect(res.status).toBe(200);
     });
 
-    it.todo('should return 200 if input is valid');
-    it.todo('should return 400 if input is not valid');
-    it.todo('should return 400 if an user was not found');
-    it.todo('should return 400 if a password is not valid');
+    it('should return 400 if input is not valid', async () => {
+        login = '';
+
+        const res = await exec();
+
+        expect(res.status).toBe(400);
+    });
+
+    it('should return 400 if an user was not found', async () => {
+        login = 'test2';
+
+        const res = await exec();
+
+        expect(res.status).toBe(400);
+    });
+
+    it('should return 400 if a password is not valid', async () => {
+        password = '111111';
+
+        const res = await exec();
+
+        expect(res.status).toBe(400);
+    });
 });
