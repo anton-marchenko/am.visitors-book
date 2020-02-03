2a2a59a1693ce94b98245369356224268e643bbb
anton-marchenko <fattan@ya.ru>
2020-02-03 15:05:38 +0300

    Feat: create sign-in route

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const users = require('./users');
 const visits = require('./visits');
 const access = require('./access');
+const signIn = require('./sign-in');
 
 router.use('/users', users);
 router.use('/visits', visits);
 router.use('/access', access);
+router.use('/sign-in', signIn);
 
 module.exports = router;
--- tests/integration/routes/sign-in.test.js
+++ tests/integration/routes/sign-in.test.js
@@ -1,6 +1,32 @@
+const request = require('supertest');
+const jwt = require('jsonwebtoken');
+const { User } = require('../../../models/user');
+
+let server;
 
 describe('/api/sign-in', () => {
-    it.todo('should return new JWT if input is valid');
+
+    beforeEach(async () => {
+        server = require('../../../index');
+    });
+
+    afterEach(async () => {
+        await server.close();
+    });
+
+    const exec = async () => {
+        return await request(server)
+            .post('/api/sign-in')
+            .send({});
+    };
+
+    it('should return new JWT if input is valid', async () => {
+        const res = await exec();
+
+        expect(res.status).toBe(200);
+    });
+
+    it.todo('should return 200 if input is valid');
     it.todo('should return 400 if input is not valid');
     it.todo('should return 400 if an user was not found');
     it.todo('should return 400 if a password is not valid');
