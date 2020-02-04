f783401e3139b66b43664b107c7d2ef9f4c26b96
anton-marchenko <fattan@ya.ru>
2020-02-04 15:14:20 +0300

    Refactor: error msg

--- routes/visits.js
+++ routes/visits.js
@@ -4,12 +4,14 @@ const { User } = require('../models/user');
 const { Visit } = require('../models/visit');
 const { thirdPartyAppAuth } = require('../middleware');
 
+const NOT_FOUND_MSG = 'An user has not been found for given card ID.';
+
 router.post('/', thirdPartyAppAuth, async (req, res) => {
     const { cardId } = req.body;
 
     const user = await User.findOne({ cardId });
 
-    if (!user) return res.status(404).send('An user has not been found for given card ID.');
+    if (!user) return res.status(404).send(NOT_FOUND_MSG);
 
     const visit = await Visit.createNewVisit(user);
 
