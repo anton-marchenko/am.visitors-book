5de2d09ce83b66ffc656dbd61373585f68364ded
anton-marchenko <fattan@ya.ru>
2020-02-04 14:43:34 +0300

    Refactor: thirdPartyPath

--- routes/access.js
+++ routes/access.js
@@ -5,12 +5,13 @@ const { ThirdPartyAccess } = require('../models/third-party-access');
 
 const NOT_FOUND_MSG = 'An access entry with the given ID was not found';
 const tokenDeleteMiddleware = [auth, allowedFor('admin'), validateObjectId];
+const thirdPartyPath = '/third-party-app/tokens';
 
 router.post('/tokens/validate', auth, async (req, res) => {
     return res.status(200).send('OK');
 });
 
-router.post('/third-party-app/tokens', [auth, allowedFor('admin')], async (req, res) => {
+router.post(thirdPartyPath, [auth, allowedFor('admin')], async (req, res) => {
     const { _id, appName, createdBy } = await ThirdPartyAccess.createNewAccess({
         createdBy: req.user._id,
         appName: req.body.appName,
@@ -25,7 +26,7 @@ router.post('/third-party-app/tokens', [auth, allowedFor('admin')], async (req, 
     return res.status(201).send(token);
 });
 
-router.delete('/third-party-app/tokens/:id', tokenDeleteMiddleware, async (req, res) => {
+router.delete(thirdPartyPath + '/:id', tokenDeleteMiddleware, async (req, res) => {
     const accessEntry = await ThirdPartyAccess.findByIdAndRemove(req.params.id);
 
     if (!accessEntry) return res.status(404).send(NOT_FOUND_MSG);
