631222e0d2d3966d0530b36c8b7b966b8602fc7e
anton-marchenko <fattan@ya.ru>
2020-02-04 11:00:42 +0300

    Feat: add third party app tokens creating

--- routes/access.js
+++ routes/access.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const { auth } = require('../middleware');
+const { auth, allowedFor } = require('../middleware');
+const { ThirdPartyAccess } = require('../models/third-party-access');
 
 router.post('/tokens/validate', auth, async (req, res) => {
     return res.status(200).send('OK');
 });
 
-router.post('/third-party-app/tokens', async (req, res) => {
-    return res.status(200).send('OK');
+router.post('/third-party-app/tokens', [auth, allowedFor('admin')], async (req, res) => {
+    const { _id, appName, createdBy } = await ThirdPartyAccess.createNewAccess({
+        createdBy: req.user._id,
+        appName: req.body.appName,
+    });
+
+    const token = new ThirdPartyAccess({
+        _id,
+        appName,
+        createdBy
+    }).generateAuthToken();
+
+    return res.status(201).send(token);
 });
 
 module.exports = router;
--- tests/integration/routes/access.test.js
+++ tests/integration/routes/access.test.js
@@ -1,8 +1,20 @@
 const request = require('supertest');
+const config = require('config');
 const jwt = require('jsonwebtoken');
 const { User } = require('../../../models/user');
+const { ThirdPartyAccess } = require('../../../models/third-party-access');
 
 let server;
+
+const mockUserData = () => ({
+    name: {
+        first: 'test',
+        patronymic: 'test',
+        last: 'test'
+    },
+    password: '12345',
+    login: 'test'
+});
 
 describe('/api/access/tokens/validate', () => {
 
@@ -65,30 +77,87 @@ describe('/api/access/third-party-app/tokens', () => {
     });
 
     describe('POST /', () => {
-        let token;
+        let token, userId, appName;
 
         beforeEach(async () => {
             server = require('../../../index');
+
+            const user = new User({
+                ...mockUserData(),
+                roles: ['admin']
+            });
+
+            await user.save();
+
+            userId = user._id;
+            token = user.generateAuthToken();
+            appName = 'test';
         });
 
         afterEach(async () => {
             await server.close();
+            await User.deleteMany({});
+            await ThirdPartyAccess.deleteMany({});
         });
 
         const exec = async () => {
             return await request(server)
                 .post('/api/access/third-party-app/tokens')
-                .set('x-auth-token', token);
+                .set('x-auth-token', token)
+                .send({ appName });
         };
 
-        beforeEach(() => {
-            token = new User().generateAuthToken();
+        it('should return new third party app token', async () => {
+            const res = await exec();
+
+            expect(() => {
+                jwt.verify(res.text, config.get('jwtSecret'))
+            }).not.toThrow();
         });
 
-        it.todo('should return new third party app token');
-        it.todo('should return 400 if clients token is not valid');
-        it.todo('should return 401 if a client is not logged in');
-        it.todo('should return 403 if permission denied');
+        it('should store an access entry to DB', async () => {
+            await exec();
+
+            const accessEntry = await ThirdPartyAccess.findOne({
+                createdBy: userId,
+                appName
+            });
+
+            expect(accessEntry).toBeTruthy();
+        });
+
+        it('should return 201 if token was created', async () => {
+            const res = await exec();
+
+            expect(res.status).toBe(201);
+        });
+
+        it('should return 400 if clients token is not valid', async () => {
+            token = 'test';
+
+            const res = await exec();
+
+            expect(res.status).toBe(400);
+        });
+
+        it('should return 401 if a client is not logged in', async () => {
+            token = '';
+
+            const res = await exec();
+
+            expect(res.status).toBe(401);
+        });
+
+        it('should return 403 if permission denied', async () => {
+            token = new User({
+                ...mockUserData(),
+                roles: []
+            }).generateAuthToken();
+
+            const res = await exec();
+
+            expect(res.status).toBe(403);
+        });
     });
 
     describe('DELETE /:accessId', () => {
